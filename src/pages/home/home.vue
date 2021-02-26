d326f5de3a302fe66f90ae44b123f27fc612e546
wxyforgithub <wxyforgithub@163.com>
2021-02-26 17:43:33 +0800

    修改burn为BT

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -153,12 +153,12 @@
           </div>
           <div class="text1 alignLeft">
             可用余额
-            <span>{{ balance }}</span>burn
+            <span>{{ balance }}</span>BT
           </div>
           <div class="input-box space-between">
             <input type="text" class="input" value placeholder="输入燃烧数量" v-model="amount" />
             <div class="align-center">
-              <div class="text2">burn</div>
+              <div class="text2">BT</div>
               <div class="line"></div>
               <div class="text3" @click="inputAll">全部</div>
             </div>
@@ -197,7 +197,7 @@
           </div>
           <div class="text5" v-if="receiveAble">
             <span class="hugeMainTxt">{{ expectAmount }}</span>
-            <br />burn
+            <br />BT
           </div>
           <div class="time-box" v-else>
             <div class="time" v-for="(item, index) in hour" :key="'1_' + index">{{ item }}</div>
